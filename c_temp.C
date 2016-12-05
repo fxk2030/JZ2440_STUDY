4bbb833ea687c75f08195fd1f2ea3023b35c1d7c
fxk <fxk2030@qq.com>
2016-12-05 18:27:55 +0800

    led

---
+++ c_temp.C
@@ -0,0 +1,10 @@
+/****************************************************************
+*Copyright (C) ,2016 fxk
+*File name:	led_on.C
+*Aunthor:	fxk       Version: V0.1    Date:2016.12.05
+*Description: This is my first JZ2440 assembly code.	  				 	
+*Function List:  
+*History: 
+*<author>  <time>  <version>  <desc>
+*
+*****************************************************************/
